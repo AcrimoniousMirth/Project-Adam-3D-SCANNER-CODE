02f0ceeba43c579bb59b86eaa508659d2ef0bc98
Acrimonious Mirth <Samuel_maybury@yahoo.co.uk>
2018-03-02 19:22:16 +0000

    Iterates through directory
    Updated the file to iterate through the directory of images provided by the scanner. This now outputs a single array **allPoints** of form 
        [[x1 y1 z1][x2 y2 z2]... [xn yn zn]]
    This is so that the array can then be triangulated into an STL.
    *Still need to apply the distortion correction multipliers*

--- I2LTEST4.py
+++ I2LTEST4.py
@@ -1,11 +1,22 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-image = cv.imread('TestImage.png', cv.IMREAD_GRAYSCALE)
-newImage = image.copy()
+import os
 
+DIRECTORY = '/home/pi/Desktop/ScannerDev/'
+
+#image = cv.imread('TestImage.png', cv.IMREAD_GRAYSCALE)
+
+
+zVal = 0          # Third dimension of all points in image
+allPoints = []    # List of all points in all images
+
+
+
+numMoves = 1      # FOR EXAMPLE
 
 def imageToPoints (image):
+# Takes an image and appends to allPoints list with given z value
     ret, discrImage = cv.threshold (image, 127, 255, 0)
     height, width = discrImage.shape
     
@@ -17,38 +28,67 @@ def imageToPoints (image):
     
     img, vectImage, heirarchy = cv.findContours(discrImage, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     
-    return vectImage
+    # from all the contours, extract the largest based off of the area
+    contImage = max(vectImage, key=cv.contourArea)
     
     
+    # Append all dimensions of point to list of all points
+    for point in contImage:
+        xVal, yVal = tuple(point[0])
+        """allPoints[i][0] = float(xVal)
+        allPoints[i][1] = float(yVal)
+        allPoints[i][2] = float(zVal)"""
+        if xVal > 3:
+        # Skip top line of the image (white values written previously)
+            point3D = (float(xVal), float(yVal), float(zVal))
+    
+            allPoints.append(point3D)
     
     
-contImage = imageToPoints(image)
-# from all the contours, extract the largest based off of the area
-contour = max(contImage, key=cv.contourArea)
+    return
+    
 
-"""
-print 'contour: ', contour
-print 'contour[1]: ', contour[1]
-print 'contour[1][0]: ', contour[1][0]
-print 'contour[1][0][0]: ', contour[1][0][0]
-print 'Length: ', len(contour)
-"""
-zVal = 1
-length = len(contour)
 
-allPoints = []
 
-for point in contour:
-    xVal, yVal = tuple(point[0])
-    """allPoints[i][0] = float(xVal)
-    allPoints[i][1] = float(yVal)
-    allPoints[i][2] = float(zVal)"""
-    point3D = (float(xVal), float(yVal), float(zVal))
-    
-    allPoints.append(point3D)
-    
-    
+def imagesToList():
+# Iterates through directory and processes each image into array
+
+    for img in os.listdir(DIRECTORY + 'TestPhotos/'):
+        # Filters out any rogue files (e.g. hidden)
+        if img.endswith(".png"):
+            image = cv.imread(DIRECTORY + 'TestPhotos/' + img, cv.IMREAD_GRAYSCALE)
+            #print image
+            if (image == None): 
+                print(DIRECTORY+'TestPhotos/'+img, "could not be read!")
+                return # can't go on with an invalid image
+            print img
+
+            camNum, imgNum = os.path.splitext(img)[0].split('img')
+            # print 'Camera is ', camNum
+            # print 'Image is ', imgNum
+        
+            # Correction of values will be different for cams 1 & 2
+            if camNum == 1:
+                # Correction values here
+                
+                zVal = imgNum*numMoves
+                imageToPoints(image)
+                
+            
+            else: #camNum == 2:
+                # Correction values here
+            
+                zVal = imgNum*numMoves
+                imageToPoints(image)
+                
+            
+        else: print '''not of format [x]img[y].png''' 
+
+
+imagesToList()
 print np.array(allPoints)
+
+
 
 
 
